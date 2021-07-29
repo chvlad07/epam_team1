312dfac51f400e06d2fa7f18ce7556028f29350c
peterlviv2020 <75026926+peterlviv2020@users.noreply.github.com>
2021-07-29 13:11:29 +0300

    Update DimDate.sql

--- AWS_RDS/DimDate.sql
+++ AWS_RDS/DimDate.sql
@@ -2,7 +2,7 @@ SET @first_date = '1970-01-01';
 SET @last_date = '2100-12-31';
 SET SESSION cte_max_recursion_depth = 1000000; 
 
-INSERT INTO DimDate
+
 WITH RECURSIVE date_dim(now_date) AS  
 	(
 	SELECT
@@ -18,22 +18,20 @@ WITH RECURSIVE date_dim(now_date) AS
     
     SELECT
     now_date AS CurrentDate,
-	FORMAT(now_date,'dd/MM/yyyy') AS EuropeanDate,
-	FORMAT(now_date,'MM/dd/yyyy') AS AmericanDate,
-    EXTRACT(DAY FROM now_date) AS NumberDay,
-	WEEKDAY(now_date) AS NumberDayOfWeek,
+    FORMAT(now_date,'dd/MM/yyyy') AS EuropeanDate,
+    FORMAT(now_date,'MM/dd/yyyy') AS AmericanDate,
+    DAY(now_date) AS NumberDay,
+    WEEKDAY(now_date) AS NumberDayOfWeek,
     DAYNAME(now_date) AS TitleOfDay,
     DAYOFYEAR(now_date) AS NumberDayOfYear,
-    EXTRACT(WEEK FROM now_date) AS NumberWeekInYear,
+    WEEK(now_date) AS NumberWeekInYear,
     WEEK(now_date,5) - WEEK(DATE_SUB(now_date, INTERVAL DAYOFMONTH(now_date) - 1 DAY), 5) + 1 as NumberWeekInMonth,
-    EXTRACT(MONTH FROM now_date) AS NumberMonth,
-    EXTRACT(MONTH FROM now_date) AS NameMonth,
-    EXTRACT(QUARTER FROM now_date) AS NumberQuarter,   
-    EXTRACT(YEAR FROM now_date) AS CurrentYear
+    MONTH(now_date) AS NumberMonth,
+    MONTHNAME(now_date) AS NameMonth,
+    QUARTER(now_date) AS NumberQuarter,   
+    YEAR(now_date) AS CurrentYear
     
 FROM
 	date_dim;
 
 SELECT * FROM DimDate
-
-
